a632437f3b01fa91f2dbf11821ec46fb090f814e
mdb571 <r.mohammedbilal@gmail.com>
2020-04-19 00:28:30 +0530

    gzip commntd

--- base_app/settings.py
+++ base_app/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGIN_URL = '/login/'
