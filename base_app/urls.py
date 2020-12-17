faa07cafebde6d5c850f1d13f861340328ea6178
mdb571 <r.mohammedbilal@gmail.com>
2020-12-17 17:46:18 +0530

    sec

--- base_app/urls.py
+++ base_app/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('nimda/', admin.site.urls),
     path('accounts/', include('allauth.urls')), # <--
     path('', include('treasurehunt.urls')),
     path('',include('pwa.urls')),
